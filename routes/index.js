24069b059cb2f6d909ba0d28e0b82dfa51c664b1
anteater@mac <anteater1056@gmail.com>
2024-06-08 12:14:34 +0900

    Controller

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,40 @@
 var express = require("express");
 var router = express.Router();
 
-const { PrismaClient } = require("@prisma/client");
+const {
+  getAllPints,
+  addNewPint,
+  deletePint,
+} = require("../services/pintService");
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  const prisma = new PrismaClient();
-  const pints = await prisma.pint.findMany();
+  const pints = await getAllPints();
+
   res.render("index", { title: "Pints!", pints: pints });
+});
+
+/** POST new pint */
+router.post("/", async function (req, res, next) {
+  const result = await addNewPint({
+    name: req.body.name,
+    rule: req.body.rule,
+    url: req.body.url,
+  });
+
+  if (!result)
+    res.render("error", {
+      message: "internal error",
+      error: { status: "500", stack: "" },
+    });
+  else res.redirect("/");
+});
+
+/** delete pint by name */
+router.post("/deletePint", async function (req, res, next) {
+  await deletePint(req.body.name);
+
+  res.redirect("/");
 });
 
 module.exports = router;
